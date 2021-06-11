97864fef2f284c10cec37cd25c1288dbc4814b5d
Charlie Fish <fishcharlie@me.com>
2021-06-11 17:54:05 -0600

    Fixing issue in dnsmasq generation script where “/“ would exist on header lines

--- scripts/generate-dnsmasq.js
+++ scripts/generate-dnsmasq.js
@@ -6,9 +6,9 @@ const path = require("path");
 	await Promise.all(files.map(async (file) => { // For each file
 		const fileContents = await fs.readFile(path.join(__dirname, "..", file), "utf8"); // Get file contents as a string
 		const noIPFileContents = fileContents
+		.replaceAll(/0\.0\.0\.0 (.*?)( .*)?$/gmu, "0.0.0.0 $1/$2") // I need this line to add "/" at the end of each URL
 		.replaceAll(/^0\.0\.0\.0 /gmu, "server=/") // Replace all occurances of "0.0.0.0 " at the beginning of the line with "server=/"
 		.replaceAll(/^# 0\.0\.0\.0 /gmu, "# server=/") // Replace all occurances of "# 0.0.0.0 " at the beginning of the line with "# server=/"
-		.replaceAll(/^(.*)$/gmu, "$1/") // I need this line to add "/" at the end of each URL
 		.replace(/^# Title: (.*?)$/gmu, "# Title: $1 (NL)"); // Add (NL) to end of title
 		await fs.writeFile(path.join(__dirname, "..", "dnsmasq-version", file.replace(".txt", "-dnsmasq.txt")), noIPFileContents, "utf8"); // Write new file to `alt-version` directory
 	}));
