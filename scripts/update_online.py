40f8124492b4187b8d0574ae9dbe1906e89ea6aa
iam-py-test <axbina@outlook.com>
2021-06-30 16:52:44 -0400

    Add auto-updating online-domains list

--- scripts/update_online.py
+++ scripts/update_online.py
@@ -7,22 +7,21 @@ def arg(p=1):
   except:
     return None
 os.chdir("..")
-os.chdir('..')
 
 lists = ["porn.txt"]
 for list in lists:
-  file = open(list)
-  alt = open(list.split(".")[0] + "_lite.txt")
-  lines = file.read().split("\n")
-  for line in lines:
-    if line.startswith("#") or "127.0.0.1" not in line:
-      alt.write("{}\n".format(line))
-      continue
-    domain = line.split(" ")[1]
-    try:
-      socket.gethostbyname(domain)
-    except:
-      pass
-    else:
-      alt.write("127.0.0.1 {}".format(domain))
-  alt.close()
+    file = open(list)
+    alt = open(list.split(".")[0] + "_lite.txt","w")
+    lines = file.read().split("\n")
+    for line in lines:
+        if line.startswith("#") or "127.0.0.1" not in line:
+            alt.write("{}\n".format(line))
+            continue
+        domain = line.split(" ")[1]
+        try:
+            socket.gethostbyname(domain)
+        except:
+            pass
+        else:
+          alt.write("127.0.0.1 {}".format(domain))
+    alt.close()
