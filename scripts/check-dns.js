23f47d12110d949caef12c39d5babbd61f8967d2
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-07-01 09:50:16 -0400

    Update check-dns.js

--- scripts/check-dns.js
+++ scripts/check-dns.js
@@ -1,1 +1,34 @@
 const dns = require("dns")
+const fs = require("fs")
+
+const lists = [
+	"abuse",
+	"ads",
+	"crypto",
+	"drugs",
+	"facebook",
+	"fraud",
+	"gambling",
+	"malware",
+	"phishing",
+	"piracy",
+	"porn",
+	"ransomware",
+	"redirect",
+	"scam",
+	"tiktok",
+	"torrent",
+	"tracking",
+
+	// The following lists are in beta and therefore not included in the everything list:
+
+	// "smart-tv",
+	// "basic",
+	// "whatsapp"
+];
+
+for(var t = 0;t < lists.length;t++){
+  fs.readFile("../" + lists[t],function(err,data){
+    console.log(data)
+  })
+}
