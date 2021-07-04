cb5e125291235c29f19c14d4a4705d972a3b7858
Charlie Fish <fishcharlie@me.com>
2021-07-04 11:56:23 -0600

    Checking to ensure all URLs are lowercase in linter

--- scripts/lint.js
+++ scripts/lint.js
@@ -9,9 +9,20 @@ const path = require("path");
 		const fileContents = await fs.readFile(path.join(__dirname, "..", file), "utf8"); // Get file contents as a string
 
 		fileContents.split("\n").forEach((line, index) => {
+			// Ensuring that all lines start with "#" or "0.0.0.0 "
 			if (line.length > 0 && !line.startsWith("#") && !line.startsWith("0.0.0.0 ")) {
 				console.error(`Line ${index + 1} in ${file} must start with "#" or "0.0.0.0 ".`);
 				hasError = true;
+			}
+
+			// Checking to ensure all URLs are lowercase
+			if (line.startsWith("0.0.0.0 ")) {
+				const lineNoIP = line.replace("0.0.0.0 ", "");
+				const url = lineNoIP.split("#")[0].trim();
+				if (url.toLowerCase() !== url) {
+					console.error(`Line ${index + 1} in ${file} url ${url} must be all lowercase.`);
+					hasError = true;
+				}
 			}
 		});
 	}));
